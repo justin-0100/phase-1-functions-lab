5af9b1a87a90d45e7f774fd0e95dfee78331e4a7
Justin <justinagui988@gmail.com>
2025-03-24 08:33:11 +0300

    distance and fare calculation

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(pickupLocation) {
+    const hqLocation = 42; 
+    return Math.abs(pickupLocation - hqLocation);
+}
+
+function distanceFromHqInFeet(pickupLocation) {
+    return distanceFromHqInBlocks(pickupLocation) * 264;
+}
+
+function distanceTravelledInFeet(start, destination) {
+    return Math.abs(destination - start) * 264;
+}
+
+function calculatesFarePrice(start, destination) {
+    const distanceInFeet = distanceTravelledInFeet(start, destination);
+
+    if (distanceInFeet <= 400) {
+        return 0; 
+    } else if (distanceInFeet > 400 && distanceInFeet <= 2000) {
+        return (distanceInFeet - 400) * 0.02;
+    } else if (distanceInFeet > 2000 && distanceInFeet <= 2500) {
+        return 25; 
+    } else {
+        return 'cannot travel that far';
+    }
+}
